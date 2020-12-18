78f832abc81eb7e644b81f225e6fbf235804f341
wyattbarber <wyattjbarber@gmail.com>
2020-12-17 20:26:07 -0500

    Removed use of default Time constructor and reassigning of Time fields

--- WakeLight.h
+++ WakeLight.h
@@ -2,141 +2,236 @@
 #define _WAKE_LIGHT_H
 
 #include <limits.h>
-#include <NTPClient.h>
-#include <WiFiUdp.h>
-#include <ros.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
 
-namespace Alarm {
+namespace Alarm
+{
 
-    const char DaysOfWeekStr[7][12] {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    enum DaysOfWeekNum{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday};
-    inline DaysOfWeekNum operator++(DaysOfWeekNum& day, int) {
-        if(day == Saturday){
+    const char DaysOfWeekStr[7][12]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    enum DaysOfWeekNum
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    };
+    inline DaysOfWeekNum operator++(DaysOfWeekNum &day, int)
+    {
+        if (day == Saturday)
+        {
             return Sunday;
         }
-        else {
-             return DaysOfWeekNum(day+1);
-        }    
+        else
+        {
+            return DaysOfWeekNum(day + 1);
+        }
     }
-    inline DaysOfWeekNum operator--(DaysOfWeekNum& day, int) {
-        if(day == Sunday){
+    inline DaysOfWeekNum operator--(DaysOfWeekNum &day, int)
+    {
+        if (day == Sunday)
+        {
             return Saturday;
         }
-        else {
-             return DaysOfWeekNum(day-1);
+        else
+        {
+            return DaysOfWeekNum(day - 1);
         }
     }
 
-    class Time {
+    class Time
+    {
         public:
-        long int toSeconds() {
+        Time()
+        {
+            Time(Saturday, 100, 100, 100);
+        }
+        Time(DaysOfWeekNum day, int hours, int minutes, int seconds)
+        {
+            this->day = day;
+            this->hours = hours;
+            this->minutes = minutes;
+            this->seconds = seconds;
+        }
+
+        long int toSeconds()
+        {
             long int seconds = 0;
-            seconds += this->hours*60*60;
-            seconds += this->minutes*60;
+            seconds += this->hours * 60 * 60;
+            seconds += this->minutes * 60;
             seconds += this->seconds;
             return seconds;
         }
-        bool operator==(Time other) {
-            return this->day        == other.day 
-                && this->hours      == other.minutes
-                && this->minutes    == other.minutes
-                && this->seconds    == other.seconds;
+        String toString()
+        {
+            return String(DaysOfWeekStr[day]) + ":" + String(hours) + ":" + String(minutes) + ":" + String(seconds);
         }
-        bool operator>(Time other) {
-            if(this->day == other.day++) {
+        bool operator==(Time other)
+        {
+            return this->day == other.day && this->hours == other.minutes && this->minutes == other.minutes && this->seconds == other.seconds;
+        }
+        bool operator>(Time other)
+        {
+            if (this->day == other.day++)
+            {
                 return true;
             }
-            else if(this->day == other.day){
+            else if (this->day == other.day)
+            {
                 return (this->toSeconds() > other.toSeconds());
             }
-            else {
+            else
+            {
                 return false;
             }
         }
-        bool operator<(Time other) {
-            if(this->day == other.day--) {
+        bool operator<(Time other)
+        {
+            if (this->day == other.day--)
+            {
                 return true;
             }
-            else if(this->day == other.day){
+            else if (this->day == other.day)
+            {
                 return (this->toSeconds() < other.toSeconds());
             }
-            else {
+            else
+            {
                 return false;
             }
         }
-        bool operator>=(Time other) {
+        bool operator>=(Time other)
+        {
             return (*this > other) || (*this == other);
         }
-        bool operator<=(Time other) {
+        bool operator<=(Time other)
+        {
             return (*this < other) || (*this == other);
         }
-        Time operator=(Time other) {
-            Time out;
-            out.day     = other.day;
-            out.hours   = other.hours;
-            out.minutes = other.minutes;
-            out.seconds = other.seconds;
-            return out;
+        Time operator-(Time other)
+        {
+            int hours;
+            int minutes;
+            int seconds;
+            if (this->day == other.day)
+            {
+                hours = abs(this->hours - other.hours);
+                minutes = abs(this->minutes - other.minutes);
+                seconds = abs(this->seconds - other.seconds);
+            }
+            else if (other.day == (this->day++))
+            {
+                hours = abs(this->hours - (other.hours + 24));
+                minutes = abs(this->minutes - other.minutes);
+                seconds = abs(this->seconds - other.seconds);
+            }
+            else if (other.day == (this->day--))
+            {
+                hours = abs(this->hours - (other.hours - 24));
+                minutes = abs(this->minutes - other.minutes);
+                seconds = abs(this->seconds - other.seconds);
+            }
+            return Time(this->day, hours, minutes, seconds);
         }
-        Time operator-(Time other) {
-            Time out;
-            if(this->day == other.day) {
-                out.hours = abs(this->hours - other.hours);
-                out.minutes = abs(this->minutes - other.minutes);
-                out.seconds = abs(this->seconds - other.seconds);
-            }
-            else if(other.day == (this->day++)){
-                out.hours = abs(this->hours - (other.hours + 24));
-                out.minutes = abs(this->minutes - other.minutes);
-                out.seconds = abs(this->seconds - other.seconds);
-            }
-            else if(other.day == (this->day--)){
-                out.hours = abs(this->hours - (other.hours - 24));
-                out.minutes = abs(this->minutes - other.minutes);
-                out.seconds = abs(this->seconds - other.seconds);
-            }
-            return out;
-        }
+
         DaysOfWeekNum day;
         int hours;
         int minutes;
         int seconds;
     };
 
-    class WakeLight {
-        public:
-        WakeLight(){}
-        ~WakeLight(){}
+    enum State
+    {
+        WAIT,
+        WAKE,
+        ALARM,
+        ERROR
+    };
 
-        void setDefaultAlarm(Time alarm){
+    typedef struct AlarmStatus
+    {
+        AlarmStatus(State state, Time time, Time nextAlarm){
+            this->state = state;
+            this->time = time;
+            this->nextAlarm = nextAlarm;
+        }
+        State state;
+        Time time;
+        Time nextAlarm;
+    } AlarmStatus;
+
+    class WakeLight
+    {
+    public:
+        // Alarm::WakeLight
+        WakeLight()
+        {
+            maxRed = 1000;
+            maxGrn = 100;
+            maxBlu = 400;
+            maxWht = 800;
+            state = WAIT;
+
+            defaults[0] = Time(Sunday, 100, 100, 100);
+            defaults[1] = Time(Monday, 6, 0, 0);
+            defaults[2] = Time(Tuesday, 6, 0, 0);
+            defaults[3] = Time(Wednesday, 6, 0, 0);
+            defaults[4] = Time(Thursday, 6, 0, 0);
+            defaults[5] = Time(Friday, 6, 0, 0);
+            defaults[6] = Time(Saturday, 100, 100, 100);
+
+            tempAlarm   = Time(Saturday, 100, 100, 100);
+
+            currentTime = Time(Saturday, 100, 100, 100);
+            networkBaseTime = Time(Saturday, 100, 100, 100);
+
+        };
+
+        void setDefaultAlarm(Time alarm)
+        {
             defaults[alarm.day] = alarm;
         }
-        void setTempAlarm(Time alarm){
+        void setTempAlarm(Time alarm)
+        {
             tempAlarm = alarm;
         }
-        void setBaseTime(Time time){
+        void setBaseTime(Time time)
+        {
             networkBaseTime = time;
         }
-        void update(){
-            // update time 
+        void pressButton()
+        {
+            // set button true, but only if the alarm is currently going off
+            button = (state == ALARM);
+        }
+        AlarmStatus getStatus() { return AlarmStatus(state, currentTime, getCurrentAlarm()); }
+        void update()
+        {
+            // update time
             updateInternalTime();
             // alarm clock state machine
-            switch(state) {
-                case WAIT:
-                // light and buzzer are off
+            switch (state)
+            {
+            case WAIT:
+                // lights are off
                 rgbw[0] = 0;
                 rgbw[1] = 0;
                 rgbw[2] = 0;
                 rgbw[3] = 0;
 
                 // check if wake sequence should start
-                if(currentTime >= getLightTime()) {
+                if (currentTime >= getLightTime())
+                {
                     state = WAKE;
                 }
 
-                case WAKE:
+            case WAKE:
                 // calculate progress of 30 minute wake fade, percentage from 0 to 1.
-                float progress = ((currentTime - getLightTime()).toSeconds()) / (30 * 60);
+                progress = ((currentTime - getLightTime()).toSeconds()) / (30 * 60);
                 // augment progress curve
                 progress = sqrt(progress);
                 // calculate rgbw values
@@ -146,85 +241,134 @@ namespace Alarm {
                 rgbw[3] = progress * maxWht;
 
                 // check if alarm time has been reached
-                if(currentTime >= getCurrentAlarm()){
+                if (currentTime >= getCurrentAlarm())
+                {
                     state = ALARM;
                 }
                 break;
-            
-                case ALARM:
-                // blink lights and buzzer
 
-                // check for button press and bed empty
+            case ALARM:
+                // blink lights
+                if (blink)
+                {
+                    rgbw[0] = maxRed;
+                    rgbw[1] = maxGrn;
+                    rgbw[2] = maxBlu;
+                    rgbw[3] = maxWht;
+                }
+                else
+                {
+                    rgbw[0] = 0;
+                    rgbw[1] = 0;
+                    rgbw[2] = 0;
+                    rgbw[3] = 0;
+                }
+                if ((millis() % 1000) == 0)
+                {
+                    ~blink;
+                }
 
-                case ERROR:
+                // check for button press
+                if (button)
+                {
+                    button = false;
+                    state = WAIT;
+                }
+
+                break;
+            case ERROR:
                 // identify and handle error
                 // not sure what errors, if any, this class can encounter
-
+                break;
             }
         }
-        uint8* getRGBWLevels(){return rgbw;}
+        int *getRGBWLevels() { return rgbw; }
 
-        private:
-        void updateInternalTime(){
+        void updateInternalTime()
+        {
             // update time since last update
             long int currMillis = millis();
             int diff;
-            if(currMillis < prevMillis){
+            if (currMillis < prevMillis)
+            {
                 diff = currMillis + (LONG_MAX - prevMillis);
             }
-            else {
+            else
+            {
                 diff = currMillis - prevMillis;
             }
+            prevMillis = currMillis;
 
             // calculate h, m, s differences
-            int seconds = (diff/1000)%60;
-            int minutes = seconds/60;
-            int hours   = minutes/60;
+            int seconds = (diff / 1000) % 60;
+            int minutes = seconds / 60;
+            int hours = minutes / 60;
+            DaysOfWeekNum day;
+
+            Serial.print("Internal: \n");
+            Serial.print("      Hours: ");
+            Serial.print(hours);
+            Serial.print("\n    Minutes: ");
+            Serial.print(minutes);
+            Serial.print("\n    Seconds: ");
+            Serial.print(seconds);
+            Serial.print("\n\n");
 
             // update current time
-            currentTime.seconds += seconds;
-            if(currentTime.seconds > 59) {
-                currentTime.minutes += 1;
-                currentTime.seconds -= 60;
+            seconds += currentTime.seconds;
+            if (seconds > 59)
+            {
+                minutes += 1;
+                seconds -= 60;
             }
-            currentTime.minutes += minutes;
-            if(currentTime.minutes > 59) {
-                currentTime.hours += 1;
-                currentTime.minutes -= 60;
+            minutes += currentTime.minutes;
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
             }
-            currentTime.hours += hours;
-            if(currentTime.hours > 23) {
-                currentTime.hours -= 24;
-                if(currentTime.day == Saturday){
-                    currentTime.day = Sunday;
+            hours += currentTime.hours;
+            if (hours > 23)
+            {
+                hours -= 24;
+                if (currentTime.day == Saturday)
+                {
+                    day = Sunday;
                 }
-                else {
-                    currentTime.day = DaysOfWeekNum(currentTime.day+1);
+                else
+                {
+                    day = DaysOfWeekNum(currentTime.day + 1);
                 }
             }
+            currentTime = Time(day, hours, minutes, seconds);
         }
-        Time getCurrentAlarm(){
+        Time getCurrentAlarm()
+        {
             Time todaysDefault = defaults[currentTime.day];
-            if(useTempAlarm && (tempAlarm.day == todaysDefault.day)) {
+            if (useTempAlarm && (tempAlarm.day == todaysDefault.day))
+            {
                 return tempAlarm;
             }
-            else {
+            else
+            {
                 return todaysDefault;
             }
         }
-        Time getLightTime(){
+        Time getLightTime()
+        {
             Time lightTime = getCurrentAlarm();
             lightTime.minutes -= 30;
-            if(lightTime.minutes < 0) {
-                lightTime.day--;
+            if (lightTime.minutes < 0)
+            {
+                lightTime.hours--;
+                lightTime.minutes += 60;
             }
             return lightTime;
         }
-        
+
         // status and IO variables
-        static enum State {WAIT, WAKE, ALARM, ERROR};
         State state;
-        uint8 rgbw[4];
+        int rgbw[4];
         // Alarm settings
         Time defaults[7];
         Time tempAlarm;
@@ -237,38 +381,41 @@ namespace Alarm {
         // WAIT
 
         // WAKE
-        uint8 maxRed;
-        uint8 maxGrn;
-        uint8 maxBlu;
-        uint8 maxWht;
+        float progress;
+        int maxRed;
+        int maxGrn;
+        int maxBlu;
+        int maxWht;
 
         // ALARM
+        bool blink;
+        bool button;
 
         // ERROR
     };
 
-};
+}; // namespace Alarm
 
-namespace SmartHome {
-namespace ROS {
-    
-        class WakeLight {
+namespace SmartHome
+{
+    namespace ROS
+    {
+
+        class WakeLight
+        {
         public:
-        WakeLight(){}
-        ~WakeLight(){}
-        
-        void update(){
-            alarmClock.update();
-        }
+            WakeLight() {}
+            ~WakeLight() {}
+
+            void update()
+            {
+                alarmClock.update();
+            }
 
         private:
-        Alarm::WakeLight alarmClock;
-
-        WiFiUDP UDPconn;
-        NTPClient timeClient(UDPconn, "pool.ntp.org", ntpOffset);
- 
-    };
-};
-};
+            Alarm::WakeLight alarmClock;
+        };
+    }; // namespace ROS
+};     // namespace SmartHome
 
 #endif
--- WakeLight.ino
+++ WakeLight.ino
@@ -1,24 +1,54 @@
 #include <ESP8266WiFi.h>
+
+#include "WakeLight.h"
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-#include "WakeLight.h"
+#define BUT_PIN         D1
+#define FIVE_V_PIN      D2
+#define TWELVE_V_PIN    D3
+#define RED_PIN         D4
+#define GRN_PIN         D5
+#define BLU_PIN         D6
+#define WHT_PIN         D7
+#define BUZ_PIN         D8
+#define MAT_PIN         A0
 
-const char * ssid = "the wifi";
-const char*  pswd = "the wifi password";
+const char *ssid = "the wifi";
+const char *pswd = "the wifi password";
 
-const long ntpOffset = 3600;
+const long ntpOffset = -18000;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", ntpOffset);
+NTPClient timeClient(ntpUDP, ntpOffset);
+Alarm::WakeLight alarmClock;
 
-void setup() {
- WiFi.begin(ssid,pswd);
- timeClient.begin();
- Serial.begin(9600);
+int* rgbw = (int*)malloc(4*sizeof(int));
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin(ssid, pswd);
+    while(WiFi.status() != WL_CONNECTED )
+    {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.print('\n');
+    timeClient.begin();
 }
 
-void loop() {
-  
-  
+void loop()
+{
+    timeClient.update();
+    if((millis() % 5000)==0)
+    {
+        alarmClock.setBaseTime(Alarm::Time((Alarm::DaysOfWeekNum)timeClient.getDay(),   
+                                            timeClient.getHours(),
+                                            timeClient.getMinutes(),
+                                            timeClient.getSeconds()));
+    }
+    alarmClock.update();
+    Serial.println(alarmClock.getStatus().time.toString()+'\n');
+    delay(1000);
 }
